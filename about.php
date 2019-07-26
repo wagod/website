f2dcc3ca2bd94fdd264a45c43175f4f0b1187d73
wagod <50874623+wagod@users.noreply.github.com>
2019-07-26 23:48:34 +0300

    about
    This is the home page code and which displays the about page

---
+++ about.php
@@ -0,0 +1,55 @@
+<html>
+<title>
+learners.com
+</title>
+
+<head >
+<table bgcolor=	"#48D1CC" width="100%"height="12%">
+<tr>
+<td align="center"><h1> LEARNERS<sup>.COM</sup> </h1></td>
+</tr>
+</table>
+</head>
+<body>
+<table align="center" width="100%"  bgcolor="#FFE4C4" height="8%">
+<tr>
+ <th >&emsp;<a href="home.php">HOME</a></th>
+ <th><a href="students.php">STUDENTS' FORUM</a></th>
+ <th><a href="tutor.php">TUTORS' FORUM</a></th>
+ <th><a href="feedback.php">FEEDBACK</a></th>
+ </tr>
+</table>
+<style>
+.container {
+    position: relative;
+    text-align: center;
+    color: yellow;
+}
+.top-left {
+    position: absolute;
+    top: 70px;
+    left: 16px;
+}
+</style>
+</head>
+<body>
+<div class="container">
+  <img src="graduation.jpg" alt="Snow" width="100%" height="100%">
+  <div class="top-left"><font align="center"size="6"><h3><u><i>About</i></u></h3><br>
+Learners.com is an online academic platform, that incoperates students of higher learning and tutors (lecturers) from verious learning institution.
+It allow students and tutors to discuss verious academic problems to find clarity and solutions via the platform.It allows a students to students 
+discussion group,tutors to tutors discussion platform and a student and a tutor's platform.It is meant to increase knowledge,skills and 
+understanding of verious academic subjects among students and tutors 
+as they share in their field of interest  <br>
+</div>
+  <table bgcolor="#778899" align="bottom"width="100%" height="15%">
+<tr>
+ <td><align="center"><b>contacts:<b></td>
+ <td><i>visit our blog page...</i><a href="http//:www.sammywagody.blogspot.com"><sub><img src="blog.jpg" width="10%" height="1.5%"></sub></a></td>
+ <td><i>find us on facebook...</i><a href="http//:www.facebook/xammywagody"><sub><img src="facebook.png" width="15%" height="2%"></sub></a></td>
+ <td><i>visit us on instagram...</i><a href="http//:www.instagram/sammywagod"><sub><img src="instagram.jpg" width="10%" height="1%"></sub></a></td>
+ <td><i>find us on twitter...</i><a href="http//:www.twiter/sammywagod"><sub><img src="twitter.png" width="15%" height="3%"></sub></a></td>
+ </tr>
+ </table>
+</body>
+</html> 
